e527d5d5278d91d8b50a9fa92c41d7a04b23029a
al7arthy <ahmdalharthy777@gmail.com>
2025-07-25 10:13:08 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    cors: true,
-    proxy: {
-      '/api/github': {
-        target: 'https://api.github.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/github/, '')
-      }
-    }
-  }
-})
+    plugins: [react(), tailwindcss()],
+    server: {
+        cors: true,
+        proxy: {
+            "/api/github": {
+                target: "https://api.github.com",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\/github/, ""),
+            },
+        },
+    },
+    base: "/portfolio",
+});
